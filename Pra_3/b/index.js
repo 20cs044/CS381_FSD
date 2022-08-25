67535d790c331e6fb840c3852d99298394ef8044
20cs044 <91063808+20cs044@users.noreply.github.com>
2022-08-25 08:27:03 +0530

    Create index.js

---
+++ Pra_3/b/index.js
@@ -0,0 +1,66 @@
+class Student{
+    constructor(firstName, lastName, age, rollNo){
+        this.firstName = firstName;
+        this.lastName  = lastName;
+        this.age       = age;
+        this.rollNo    = rollNo;
+
+    }
+    
+    setFirstName(newFirstName) {
+        if (newFirstName === '') {
+            throw 'The first name cannot be empty';
+        }
+        this.firstName = newFirstName;
+    }
+    setLastName(newLastName) {
+        if (newLastName === '') {
+            throw 'The last name cannot be empty';
+        }
+        this.lastName = newLastName;
+    }
+    setAge(newAge) {
+        if (newAge === '') {
+            throw 'The age cannot be empty';
+        }
+        this.age = newAge;
+    }
+    setrollNo(newrollNo) {
+        if (newrollNo === '') {
+            throw 'The roll no. cannot be empty';
+        }
+        this.rollNo = newrollNo;
+    }
+
+    getFirstName(){
+        return this.firstName;
+    }
+    getLastName(){
+        return this.lastName;
+    }
+    getAge(){
+        return this.age;
+    }
+    getrollNo(){
+        return this.rollNo;
+    }
+
+
+}
+
+var s1 = new Student();
+
+s1.setFirstName('Kishan');
+s1.setLastName('Patel');
+s1.setAge(20);
+s1.setrollNo('20CS052');
+
+console.log('getter for first name '+s1.getFirstName());
+console.log('getter for last name '+s1.getLastName());
+console.log('getter for age '+s1.getAge());
+console.log('getter for roll no. '+s1.getrollNo());
+console.log('\n ');
+console.log('\n ');
+console.log('20CS052_Kishankumar_patel ');
+console.log('\n ');
+console.log('\n ');
